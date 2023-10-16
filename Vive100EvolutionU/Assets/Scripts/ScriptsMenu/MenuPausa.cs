60608452d0174851093c59908ed1ea01b9272c9d
PINGUIMOCHO <77744266+ELPINGUIMAN@users.noreply.github.com>
2023-10-16 00:41:13 -0500

    Menu 2.0

---
+++ Vive100EvolutionU/Assets/Scripts/ScriptsMenu/MenuPausa.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+
+    [SerializeField] private GameObject botonPausa;
+
+    [SerializeField] private GameObject menuPausa;
+
+    public void Pausa()
+    {
+        Time.timeScale = 0f;
+        botonPausa.SetActive(false);
+        menuPausa.SetActive(true);
+    }
+
+    public void Reanudar()
+    {
+        Time.timeScale = 1f;
+        botonPausa.SetActive(true);
+        menuPausa.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+
+    }
+
+}
